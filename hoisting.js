f92850c13e5b54f3271eecfb63e044eca5d284f4
5arSanti <santiari05@hotmail.com>
2023-04-29 11:55:52 -0500

    Archivos agregados

---
+++ hoisting.js
@@ -0,0 +1,15 @@
+//*>
+console.log(miNombre)
+//
+var miNombre = "Santiago";
+
+//*>
+var miNombre = undefined;
+console.log(miNombre);
+//
+
+hey();
+
+function hey(){
+    console.log("Hola " + miNombre);
+}
---
+++ if.js
@@ -0,0 +1,6 @@
+//sintaxis
+condition ? true : false;
+
+
+var numero = 1;
+var resultado = numero === 1 ? "Si soy un uno" : "No, no soy un uno";
